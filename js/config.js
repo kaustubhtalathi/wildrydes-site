9921d6430926fbd8b98e5f335f22a4ef0e8c0a43
Kaustubh Talathi <kaustubhtalathi@gmail.com>
2021-01-01 12:22:59 -0800

    Updated config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_KoiZjqSdJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5vqle3merl38pjqodqbo8071q6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: "us-east-1_KoiZjqSdJ", // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: "5vqle3merl38pjqodqbo8071q6", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: "us-east-1", // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+        invokeUrl:
+            "https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    },
 };
