0704654fe1c04d81bb1152b252dbdca8ab5988d5
Kaustubh Talathi <kaustubhtalathi@gmail.com>
2021-01-01 14:56:09 -0800

    Added crossDomain

--- js/ride.js
+++ js/ride.js
@@ -20,7 +20,7 @@ WildRydes.map = WildRydes.map || {};
             method: 'POST',
             url: _config.api.invokeUrl + '/ride',
             headers: {
-                Authorization: authToken,
+                Authorization: authToken
             },
             crossDomain : true,
             data: JSON.stringify({
