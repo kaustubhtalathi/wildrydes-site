b3b9724552edbaf60e7614b734844dfe34727b2e
Kaustubh Talathi <kaustubhtalathi@gmail.com>
2021-01-01 12:25:39 -0800

    Updated config

--- js/config.js
+++ js/config.js
@@ -6,6 +6,6 @@ window._config = {
     },
     api: {
         invokeUrl:
-            "https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+            "https://xtiqok6k01.execute-api.us-east-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
     },
 };
